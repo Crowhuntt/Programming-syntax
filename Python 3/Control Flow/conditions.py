52ca68f9958e53b0a60200f340589eac84f6521d
Ion <ionmarijuan@gmail.com>
2025-08-21 09:13:15 +0200

    Update conditions.py #range

--- Python 3/Control Flow/conditions.py
+++ Python 3/Control Flow/conditions.py
@@ -9,16 +9,29 @@ elif x == 1:
     print('One')
 else:
     print('More than one')
-
-#for
+#######################################################################################
+#for - for statement iterates over the items of any sequence
 
 words = ['cat', 'window', 'defenestrate']
 for w in words:
     print(w, len(w))
 
 users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
-# Strategy:  Iterate over a copy
 for user, status in users:
     if status == 'inactive':
         del users[user]
+#######################################################################################
+#range - If you do need to iterate over a sequence of numbers
 
+for i in range(5):
+    print(i)
+    
+list(range(5, 10))
+[5, 6, 7, 8, 9]
+
+list(range(0, 10, 3))
+[0, 3, 6, 9]
+
+list = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(list)):
+    print(i, list[i])
