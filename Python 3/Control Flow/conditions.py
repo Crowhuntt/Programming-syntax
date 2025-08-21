8ba383b24b2bab792b2eb17234a2449052f5ed5e
Ion <ionmarijuan@gmail.com>
2025-08-21 09:18:10 +0200

    Update conditions.py #break and #continue

--- Python 3/Control Flow/conditions.py
+++ Python 3/Control Flow/conditions.py
@@ -9,6 +9,7 @@ elif x == 1:
     print('One')
 else:
     print('More than one')
+    
 #######################################################################################
 #for - for statement iterates over the items of any sequence
 
@@ -20,6 +21,7 @@ users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
 for user, status in users:
     if status == 'inactive':
         del users[user]
+        
 #######################################################################################
 #range - If you do need to iterate over a sequence of numbers
 
@@ -27,11 +29,28 @@ for i in range(5):
     print(i)
     
 list(range(5, 10))
-[5, 6, 7, 8, 9]
+# [5, 6, 7, 8, 9]
 
 list(range(0, 10, 3))
-[0, 3, 6, 9]
+# [0, 3, 6, 9]
 
 list = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(list)):
     print(i, list[i])
+    
+#######################################################################################
+# break - The break statement breaks out of the innermost enclosing for or while loop
+# continue - The continue statement continues with the next iteration of the loop
+
+for num in range(2, 6):
+    if num % 2 == 0:
+        print(f"Found an even number {num}")
+        continue
+    print(f"Found an odd number {num}")
+    break
+# Found an even number 2
+# Found an odd number 3
+
+#######################################################################################
+
+
