8ddaaa7ead94beb2b281bd3ab5d58dbe340d346d
Ion <ionmarijuan@gmail.com>
2025-08-25 08:49:24 +0200

    Document tuple properties in tuple.py
    Added comments explaining tuple properties.

--- Python 3/Data Structures/tuple.py
+++ Python 3/Data Structures/tuple.py
@@ -1,1 +1,5 @@
-#
+#tuple ()
+# Immutable: Once created, elements cannot be modified
+# Ordered: Maintains the order of elements
+# Allows Duplicates: Can contain duplicate values
+# Heterogeneous: Can store different data types
