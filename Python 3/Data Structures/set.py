4105a2d9cb490a84d0b0ea72cb3bcfe802d27c84
Ion <ionmarijuan@gmail.com>
2025-08-25 08:51:00 +0200

    Document properties of sets in set.py

--- Python 3/Data Structures/set.py
+++ Python 3/Data Structures/set.py
@@ -1,1 +1,5 @@
-#
+#set {}
+# Mutable: Elements can be added or removed
+# Unordered: Does not maintain the order of elements
+# Unique Elements: Duplicate values are automatically removed
+# Heterogeneous: Can store different data types
