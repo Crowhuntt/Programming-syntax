da02cfdb6407f80a52e431e7bc02c132cf7a3d6b
Ion <ionmarijuan@gmail.com>
2025-08-25 08:33:50 +0200

    Enhance loops.py with for loop examples

--- Python 3/Control Flow/loops.py
+++ Python 3/Control Flow/loops.py
@@ -1,1 +1,12 @@
-#
+#for - for statement iterates over the items of any sequence
+
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+for user, status in users:
+    if status == 'inactive':
+        del users[user]
+
+#######################################################################################
