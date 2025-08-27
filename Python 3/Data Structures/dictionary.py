43ac7278950da7c784a6efd55d6e562a634a2b69
Ion <ionmarijuan@gmail.com>
2025-08-27 08:52:20 +0200

    Enhance dictionary.py with examples and comments

--- Python 3/Data Structures/dictionary.py
+++ Python 3/Data Structures/dictionary.py
@@ -1,4 +1,4 @@
-#dictionary
+#dictionary {:}
 # Ordered: Maintains the order of elements
 # Mutable: Elements can be modified after creation
 # Unique Elements: Duplicate values are automatically removed
@@ -31,3 +31,69 @@ sorted(tel)
 # False
 
 #######################################################################################
+#dict() - The dict() constructor builds dictionaries directly from sequences of key-value pairs
+
+dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+# {'sape': 4139, 'guido': 4127, 'jack': 4098}
+
+#######################################################################################
+#dictionary comprehensions - dict comprehensions can be used to create dictionaries from arbitrary key and value expressions
+
+{x: x**2 for x in (2, 4, 6)}
+# {2: 4, 4: 16, 6: 36}
+
+#######################################################################################
+#looping
+#items - When looping through dictionaries, the key and corresponding value can be retrieved at the same time using the items() method
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
+# gallahad the pure
+# robin the brave
+
+#######################################################################################
+#zip - To loop over two or more sequences at the same time, the entries can be paired with the zip() function
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}?  It is {1}.'.format(q, a))
+# What is your name?  It is lancelot.
+# What is your quest?  It is the holy grail.
+# What is your favorite color?  It is blue.
+
+#######################################################################################
+#reversed - To loop over a sequence in reverse, first specify the sequence in a forward direction and then call the reversed() function
+
+for i in reversed(range(1, 6, 2)):
+    print(i)
+# 5
+# 3
+# 1
+
+#######################################################################################
+#sorted - To loop over a sequence in sorted order, use the sorted() function which returns a new sorted list while leaving the source unaltered
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for i in sorted(basket):
+    print(i)
+# apple
+# apple
+# banana
+# orange
+# orange
+# pear
+
+#######################################################################################
+#set() - Using set() on a sequence eliminates duplicate elements
+# The use of sorted() in combination with set() over a sequence is an idiomatic way to loop over unique elements of the sequence in sorted order
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(basket)):
+    print(f)
+# apple
+# banana
+# orange
+# pear
+
