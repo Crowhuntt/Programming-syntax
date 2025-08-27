2392a09e97c3098da28d971b2041a68c04b8b037
Ion <ionmarijuan@gmail.com>
2025-08-27 08:57:26 +0200

    Clean up conditions.py by removing range examples
    Removed unused range examples and related comments.

--- Python 3/Control Flow/conditions.py
+++ Python 3/Control Flow/conditions.py
@@ -38,27 +38,6 @@ for num in range(2, 6):
 # Found an odd number 3
 
 #######################################################################################
-#range - If you do need to iterate over a sequence of numbers
-
-for i in range(5):
-    print(i)
-    
-list(range(5, 10))
-# [5, 6, 7, 8, 9]
-
-list(range(0, 10, 3))
-# [0, 3, 6, 9]
-
-list = ['Mary', 'had', 'a', 'little', 'lamb']
-for i in range(len(list)):
-    print(i, list[i])
-# 0 Mary
-# 1 had
-# 2 a
-# 3 little
-# 4 lamb
-
-#######################################################################################
 #pass - The pass statement does nothing. It can be used when a statement is required syntactically but the program requires no action
 
 class EmptyClass:
