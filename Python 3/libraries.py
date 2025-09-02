475bc72f38b1598d401ea63f61f08b0ec6535d2b
Ion <ionmarijuan@gmail.com>
2025-09-02 08:40:50 +0200

    Document Python modules in libraries.py

--- Python 3/libraries.py
+++ Python 3/libraries.py
@@ -1,5 +1,46 @@
-
+#os - The os module provides dozens of functions for interacting with the operating system
 #######################################################################################
 
 
 #######################################################################################
+#glob - The glob module provides a function for making file lists from directory wildcard searches
+
+#######################################################################################
+#sys -  Common utility scripts often need to process command line arguments
+# These arguments are stored in the sys module’s argv attribute as a list
+
+#######################################################################################
+#re - The re module provides regular expression tools for advanced string processing
+
+#######################################################################################
+#math - The math module gives access to the underlying C library functions for floating-point math
+
+#######################################################################################
+#random - The random module provides tools for making random selections
+
+#######################################################################################
+#statistics - The statistics module calculates basic statistical properties (the mean, median, variance, etc.) of numeric data
+
+#######################################################################################
+#urllib.request
+#urlopen
+# There are a number of modules for accessing the internet and processing internet protocols.
+# Two of the simplest are urllib.request for retrieving data from URLs and smtplib for sending mail
+
+#######################################################################################
+#datetime - The datetime module supplies classes for manipulating dates and times in both simple and complex ways
+
+#######################################################################################
+#zlib #gzip #bz2 #lzma #zipfile #tarfile.
+# Common data archiving and compression formats
+
+#######################################################################################
+#timeit - Performance measurement tool
+
+#######################################################################################
+#doctest -  The doctest module provides a tool for scanning a module and validating tests embedded in a program’s docstrings.
+
+#######################################################################################
+#unittest - Allows a more comprehensive set of tests to be maintained in a separate file
+
+
