ff1f153c46971475cbe9edd662d668c0ed4b45c6
Ion <ionmarijuan@gmail.com>
2025-09-05 09:10:18 +0200

    Add examples of common Python exceptions
    Added examples of exceptions in Python.

---
+++ Python 3/exceptions.py
@@ -0,0 +1,13 @@
+#error
+#exception
+# Errors detected during execution are called exceptions and are not unconditionally fatal
+
+10 * (1/0)
+# ZeroDivisionError: division by zero
+
+4 + spam*3
+# NameError: name 'spam' is not defined
+
+'2' + 2
+# TypeError: can only concatenate str (not "int") to str
+
